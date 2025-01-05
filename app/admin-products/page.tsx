cd155b1644ba0cca01a9b8335bda0f119fa7192b
hamedrf <123485655+hamedrf@users.noreply.github.com>
2025-01-05 20:20:09 +0330

    create admin products page

---
+++ app/admin-products/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const AdminProducts = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const data = searchParams.get("data");
+  const user = data ? JSON.parse(data) : {};
+
+  useEffect(() => {
+    if (user.username !== "admin" && user.password !== "admin")
+      router.push(`/login`);
+  }, [router, user.password, user.username]);
+
+  return (
+    <div>
+      <div className="w-screen h-32 flex justify-center items-center font-bold text-3xl border-b-4 border-blue-600">
+        سلام {user.username} خوش امدین به پنل مدریت محصولات{" "}
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default AdminProducts;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,31 +1,52 @@
+"use client";
+import MainBtn, { colorBtn } from "@/components/UI/MainBtn";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 const Login = () => {
+  const [user, setUser] = useState({ username: "", password: "" });
+  const router = useRouter();
+
+  useEffect(() => {
+    alert("نام کاربری و رمز admin  هستش ");
+  }, []);
+  const handleNavigation = () => {
+    if (user.username === "admin" && user.password === "admin")
+      router.push(
+        `/admin-products?data=${encodeURIComponent(JSON.stringify(user))}`
+      );
+    else alert("نام کاربری یا رمز ورود اشتباه است");
+  };
   return (
     <div
       className="h-screen w-screen bg-second flex justify-center items-center"
       style={{ marginTop: "-10vh" }}
     >
-      <div className="!border-8  border-red-900 rounded-2xl bg-white ">
-        <form className="flex flex-col justify-center items-center">
+      <div className=" !border-8 !border-green-900 rounded-2xl bg-white h-2/4 w-2/4 lg:w-1/4 md:w-1/3 flex justify-center  flex-col md:px-10 ">
+        <div className="mb-16 text-center font-bold text-2xl">
+          پنل ورود مدیر
+        </div>
+        <div className="flex flex-col justify-center ">
           <input
             type="text"
             name="name"
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
             className="border rounded-2xl !py-3 !px-2 mx-2 !my-4"
             placeholder="نام"
           />
           <input
             type="password"
-            name="description"
+            name="password"
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
             className="border rounded-2xl !py-3 !px-2 mx-2 !my-4"
-            placeholder="توضیحات"
+            placeholder="رمز ورود"
           />
-          <button
-            type="submit"
-            className="bg-blue-500 text-white rounded-2xl px-4 py-2 !my-4"
-          >
-            {" "}
-            ورود
-          </button>
-        </form>
+          <MainBtn
+            text="ورود"
+            color={colorBtn.main}
+            eventClick={handleNavigation}
+          />
+        </div>
       </div>
     </div>
   );
